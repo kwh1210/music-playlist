89adb57a0b714a5eac2f88654c0c21dde1729de4
kwh1210 <kwh1210@gmail.com>
2017-07-09 20:40:28 -0500

    added CRUD to all songs and playlists

--- server.py
+++ server.py
@@ -13,14 +13,7 @@ from flask import make_response
 import requests
 
 import inspect
-def PrintFrame():
-  callerframerecord = inspect.stack()[1]    # 0 represents this line
-                                            # 1 represents line at caller
-  frame = callerframerecord[0]
-  info = inspect.getframeinfo(frame)
-  print info.filename                       # __FILE__     -> Test.py
-  print info.function                       # __FUNCTION__ -> Main
-  print info.lineno 
+
 
 app = Flask(__name__)
 
@@ -59,17 +52,206 @@ def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
-    except:
+    except BaseException:
         return None
 
+
+def findIndex(query, pid):
+    if query:
+        for idx, val in enumerate(query):
+            print(idx, val.id, pid)
+            if pid == val.id:
+                return idx
+    else:
+        return None
+
+
+# JSON APIs to view playlist Information
+@app.route('/playlist/<int:playlist_id>/JSON')
+def playlistJSON(playlist_id):
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    songs = session.query(Song).filter_by(
+        playlist_id=playlist_id).all()
+    return jsonify(playlist=[i.serialize for i in songs])
+
+
+@app.route('/playlist/<int:playlist_id>/<int:song_id>/JSON')
+def songJSON(playlist_id, song_id):
+    song = session.query(Song).filter_by(id=song_id).one()
+    return jsonify(song=song.serialize)
+
+
+@app.route('/playlist/JSON')
+def playlistsJSON():
+    playlists = session.query(Playlist).all()
+    return jsonify(playlists=[r.serialize for r in playlists])
+
+
+@app.route('/playlist/<int:playlist_id>/edit/', methods=['GET', 'POST'])
+def editPlaylist(playlist_id):
+    editedPlaylist = session.query(
+        Playlist).filter_by(id=playlist_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if editedPlaylist.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to edit this playlist. Please create your own playlist in order to edit.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        if request.form['name']:
+            editedPlaylist.name = request.form['name']
+            flash('playlist Successfully Edited %s' % editedPlaylist.name)
+        return redirect(url_for('showSongs', playlist_id=playlist_id))
+    else:
+        return render_template('editPlaylist.html', playlist=editedPlaylist)
+
+
+# Delete a playlist
+@app.route('/playlist/<int:playlist_id>/delete/', methods=['GET', 'POST'])
+def deletePlaylist(playlist_id):
+    playlistToDelete = session.query(
+        Playlist).filter_by(id=playlist_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if playlistToDelete.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to delete this playlist. Please create your own playlist in order to delete.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        session.delete(playlistToDelete)
+        flash('%s Successfully Deleted' % playlistToDelete.name)
+        session.commit()
+        return redirect(url_for('showPlaylist', playlist_id=playlist_id))
+    else:
+        return render_template(
+            'deletePlaylist.html',
+            playlist=playlistToDelete)
+
+
+# Create a new item
+@app.route('/playlist/<int:playlist_id>/new/', methods=['GET', 'POST'])
+def newSong(playlist_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    if login_session['user_id'] != playlist.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to add items to this playlist. Please create your own playlist in order to add items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        newItem = Song(
+            name=request.form['name'],
+            artist=request.form['artist'],
+            album=request.form['album'],
+            playlist_id=playlist_id,
+            user_id=playlist.user_id)
+        session.add(newItem)
+        session.commit()
+        flash(
+            'New %s Song Successfully Added to the Playlist' %
+            (newItem.name))
+        return redirect(url_for('showSongs', playlist_id=playlist_id))
+    else:
+        return render_template('newsong.html', playlist_id=playlist_id)
+
+
+@app.route('/playlist/<int:playlist_id>/<int:song_id>')
+def showSong(playlist_id, song_id):
+    song = session.query(Song).filter_by(id=song_id).one()
+    return render_template('song.html', song=song, playlist_id=playlist_id)
+
+
+@app.route('/playlist/<int:playlist_id>/')
+def showSongs(playlist_id):
+    playlists = session.query(Playlist).order_by(asc(Playlist.name))
+    if playlists:
+        pid = findIndex(playlists, playlist_id)
+    # return render_template('playlists.html', playlists=playlists)
+    playlist = playlists[pid]
+    creator = getUserInfo(playlist.user_id)
+    songs = session.query(Song).filter_by(
+        playlist_id=playlist_id).all()
+    if 'username' not in login_session or creator.id != login_session['user_id']:
+        return render_template(
+            'playlist.html',
+            songs=songs,
+            playlist=playlist,
+            creator=creator,
+            playlists=playlists)
+    else:
+        return render_template(
+            'playlist.html',
+            songs=songs,
+            playlist=playlist,
+            creator=creator,
+            playlists=playlists)
+
+
+@app.route(
+    '/playlist/<int:playlist_id>/<int:song_id>/edit',
+    methods=[
+        'GET',
+        'POST'])
+def editSong(playlist_id, song_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    if login_session['user_id'] != playlist.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to edit menu items to this playlist. Please create your own playlist in order to edit items.');}</script><body onload='myFunction()''>"
+    song = session.query(Song).filter_by(id=song_id).one()
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            song.name = request.form['name']
+        if request.form['artist']:
+            song.artist = request.form['artist']
+        if request.form['album']:
+            song.album = request.form['album']
+        session.add(song)
+        session.commit()
+        flash('Song Successfully Edited')
+        return redirect(
+            url_for(
+                'showSong',
+                playlist_id=playlist_id,
+                song_id=song_id))
+    else:
+        return render_template(
+            'editsong.html',
+            playlist_id=playlist_id,
+            song_id=song_id,
+            song=song)
+
+
+@app.route(
+    '/playlist/<int:playlist_id>/<int:song_id>/delete',
+    methods=[
+        'GET',
+        'POST'])
+def deleteSong(playlist_id, song_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    if login_session['user_id'] != playlist.user_id:
+        return "<script>function myFunction() {alert('You are not authorized to edit menu items to this playlist. Please create your own playlist in order to edit items.');}</script><body onload='myFunction()''>"
+    song = session.query(Song).filter_by(id=song_id).one()
+    playlist = session.query(Playlist).filter_by(id=playlist_id).one()
+    if request.method == 'POST':
+        session.delete(song)
+        session.commit()
+        flash('Menu Item Successfully Deleted')
+        return redirect(
+            url_for(
+                'showSongs',
+                playlist_id=playlist_id,
+                song_id=song_id))
+    else:
+        return render_template('deleteSong.html', song=song)
 
 
 @app.route('/')
 @app.route('/playlist')
-def showPlaylist(): 
-    return render_template('playlists.html')
+def showPlaylist():
+    playlists = session.query(Playlist).order_by(asc(Playlist.name))
+    return render_template('playlists.html', playlists=playlists)
 
 # Create anti-forgery state token
+
+
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
@@ -77,7 +259,6 @@ def showLogin():
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
-
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -132,8 +313,8 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -152,7 +333,6 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
-
     # see if user exists, if it doesn't make a new one
     user_id = getUserID(data["email"])
     if not user_id:
@@ -170,11 +350,12 @@ def gconnect():
     print "done!"
     return output
 
+
 @app.route('/gdisconnect')
 def gdisconnect():
     # Only disconnect a connected user.
     credentials = login_session.get('access_token')
-    print credentials    
+    print credentials
     access_token = login_session['access_token']
     print 'In gdisconnect access token is %s', access_token
     if credentials is None:
@@ -185,7 +366,7 @@ def gdisconnect():
     access_token = credentials
     print '1'
     # Execute HTTP GET request to revoke current token. (# or access_token)
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials  
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
 
@@ -195,6 +376,7 @@ def gdisconnect():
             json.dumps('Failed to revoke token for given user.'), 400)
         response.headers['Content-Type'] = 'application/json'
         return response
+
 
 @app.route('/newPlaylist', methods=['GET', 'POST'])
 def newPlaylist():
@@ -210,10 +392,12 @@ def newPlaylist():
     else:
         return render_template('newPlaylist.html')
 
+
 @app.route('/clearSession')
 def clearSession():
     login_session.clear()
     return "Session cleared"
+
 
 @app.route('/whoami')
 def whoami():
@@ -236,9 +420,7 @@ def logout():
         return redirect(url_for('showPlaylist'))
 
 
-
-
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'	
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=2000)
